1603f55f2d1b5dd6c183bbe4dfcf43eea7acf990
Inoue_Satsuki <s.inoue@otwo.jp>
2022-09-15 18:58:15 +0900

    Create AddCommand.h

---
+++ src/AddCommand/AddCommand.h
@@ -0,0 +1,14 @@
+#ifndef SIP_ADD_COMMAND_H_
+#define SIP_ADD_COMMAND_H_
+#include "../ICommand.h"
+
+namespace sip
+{
+	template< typename T >
+	class AddCommand : public ICommand
+	{
+	public:
+	};
+}
+
+#endif // !SIP_ADD_COMMAND_H_
