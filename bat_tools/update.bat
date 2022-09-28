84b93f7d8ccf8774627f5b15dcc5efa8a486fa42
Inoue_Satsuki <s.inoue@otwo.jp>
2022-09-28 18:59:43 +0900

    Update update.bat

--- bat_tools/update.bat
+++ bat_tools/update.bat
@@ -3,6 +3,11 @@ setlocal
 
 set BAT_TOOLS_DIR=%CD%
 cd %BAT_TOOLS_DIR%
+if not exist "../gyp_tools" (
+	cd ../
+	mkdir gyp_tools
+	cd %BAT_TOOLS_DIR%
+)
 if not exist "../gyp_tools/gyp" (
 	call init_gyp_tools.bat
 )
